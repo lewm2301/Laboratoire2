342852ab575f6e676730eb49a1845705db9ee4a0
TP2D <112493274+Dalton-14@users.noreply.github.com>
2022-08-30 11:02:16 -0400

    Modification de nom

---
+++ entete.sh
@@ -0,0 +1,34 @@
+
+#!/usr/bin/bash
+# -----------------------------------------------------------------------------
+# Auteur : Dalton Labrecque
+# Description: Écris une entête avec date du jour dans un fichier passé en paramètre
+#
+# Date : 2022-08-30
+# -----------------------------------------------------------------------------
+#Prend en compte le paramètre
+#Saisir le nom du fichier en paramètre
+fichier=$1
+#Définir un compteur de ligne (permettant de calculer le nombre de valeurs dans le fichier)
+compteur=1
+
+
+
+#Lis chacune des ligne du fichier
+for line in $(cat $fichier);
+do
+	#Compteur
+	if [ $compteur -eq 1 ]
+	thenn
+	compteur=$(( $compteur + 1 ))
+	max=$line
+# Si le nombre lu est plus petit que la valeur min.
+	else
+		if [ $line -gt $max ]
+		then
+		max=$line
+		fi
+	fi
+done
+echo "
+La valeur max est de $max"
